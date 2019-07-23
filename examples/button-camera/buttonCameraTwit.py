24ed116ea48e3ef63106667275d85aca54b46dd3
joe <joeshimwell@gmail.com>
2019-07-23 14:25:35 +0100

    Changes to the Camera code to simplify

--- examples/button-camera/buttonCameraTwit.py
+++ examples/button-camera/buttonCameraTwit.py
@@ -14,12 +14,8 @@
 from twython import Twython
 from twythonCreds import *
 
-# import timestamper
-from datetime import datetime
-
 # Import our the camera module
 from picamera import PiCamera
-from time import sleep
 
 # import GPIOzero for the Button
 from gpiozero import Button
@@ -28,29 +24,25 @@ from signal import pause
 # I need to change the satus each time so I don't get blocked from Twitter
 import random
 
-#initiate Twitter
+#initiate Twitter, Button and the PiCamera
 twitter = Twython (APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-# initiate the Button
 button = Button(3)
-
-# initiate the Camera
 camera = PiCamera()
 
+# Picture taker
+def snapSnap():
+    camera.capture('camImage.jpg')
+    print("Snap")
+    tweetImage()
+
 # Media Upload Function
-def mediaUp():
+def tweetImage():
     photo = open('camImage.jpg', 'rb')
     response = twitter.upload_media(media=photo)
     ranNum = str(random.randint(1,101))
     twitter.update_status(status=ranNum, media_ids=[response['media_id']])
     print("Twitter image uploaded successfully")
     photo.close()
-    
-# Picture taker
-def snapSnap():
-    camera.capture('camImage.jpg')
-    print("Snap")
-    mediaUp()
         
 if __name__ == "__main__": 
     button.when_pressed = snapSnap
