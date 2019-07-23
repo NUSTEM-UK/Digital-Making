c9b00905ab19131b043a968b5e5d4ae9e796a236
Jonathan Sanderson <lists@quernstone.com>
2019-07-23 12:55:23 +0100

    Flappy bird stuff

---
+++ examples/pgzero/pgzerotest.py
@@ -0,0 +1,37 @@
+"""PyGame Zero test script.
+
+Run using:
+
+    pgzrun pgzerotest.py
+
+Will pop up a window with a dark red background and an alien graphic. Note that PyGameZero is broken on Macs as of Summer 2019.
+"""
+
+alien = Actor('alien')
+alien.pos = 0, 10
+
+WIDTH = 300
+HEIGHT = alien.height + 20
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_alien_hurt()
+    else:
+        print("You missed me!")
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.eep.play()
+    clock.schedule_unique(set_alien_normal, 1.0)
+
+def set_alien_norma():
+    alien.image = 'alien'
+
+def draw():
+    screen.fill((128, 0, 0))
+    alien.draw()
