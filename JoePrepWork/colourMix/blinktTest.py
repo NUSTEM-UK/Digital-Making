a3c247d405265bd12f4e48d14d2ddc3b879edf4b
joe <joeshimwell@gmail.com>
2019-07-23 15:26:47 +0100

    Some more playing with blinkt and input buttons

--- JoePrepWork/colourMix/blinktTest.py
+++ JoePrepWork/colourMix/blinktTest.py
@@ -1,5 +1,6 @@
 from blinkt import set_pixel, set_brightness, show, clear
 import time
+from gpiozero import DigitalInputDevice
 
 set_brightness(0.1)
 
@@ -9,3 +10,15 @@ while True:
         set_pixel(i, 255, 0, 0)
         show()
         time.sleep(0.5)
+
+
+# I need 3 switches - blue, red and green
+# that give 0/1 for colour
+https://gpiozero.readthedocs.io/en/stable/api_input.html#base-classes
+
+blueB = DigitalInputDevice(2)
+redB = DigitalInputDevice(3)
+yellowB = DigitalInputDevice(4)
+
+# to go in loop
+blueVal = blueB.value
