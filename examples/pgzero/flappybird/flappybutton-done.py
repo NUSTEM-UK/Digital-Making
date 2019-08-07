e74ea71e5b0251ba0adb436ef81270c14a9e6362
Jonathan Sanderson <lists@quernstone.com>
2019-08-07 16:41:05 +0100

    PyGame zero docs

---
+++ examples/pgzero/flappybird/flappybutton-done.py
@@ -0,0 +1,105 @@
+import pgzrun
+import random
+from gpiozero import Button
+
+
+TITLE = 'Flappy Bird'
+WIDTH = 400
+HEIGHT = 708
+
+# These constants control the difficulty of the game
+GAP = 170
+GRAVITY = 0.15
+FLAP_STRENGTH = 4.0
+SPEED = 3
+
+bird = Actor('bird1', (75, 200))
+bird.dead = False
+bird.score = 0
+bird.vy = 0
+
+button = Button(26)
+
+def reset_pipes():
+    pipe_gap_y = random.randint(200, HEIGHT - 200)
+    pipe_top.pos = (WIDTH, pipe_gap_y - GAP // 2)
+    pipe_bottom.pos = (WIDTH, pipe_gap_y + GAP // 2)
+
+
+pipe_top = Actor('top', anchor=('left', 'bottom'))
+pipe_bottom = Actor('bottom', anchor=('left', 'top'))
+reset_pipes()  # Set initial pipe positions.
+
+
+def update_pipes():
+    pipe_top.left -= SPEED
+    pipe_bottom.left -= SPEED
+    if pipe_top.right < 0:
+        reset_pipes()
+        if not bird.dead:
+            bird.score += 1
+
+
+def update_bird():
+    uy = bird.vy
+    bird.vy += GRAVITY
+    bird.y += (uy + bird.vy) / 2
+    bird.x = 75
+
+    if not bird.dead:
+        if bird.vy < -3:
+            bird.image = 'bird2'
+        else:
+            bird.image = 'bird1'
+
+    if bird.colliderect(pipe_top) or bird.colliderect(pipe_bottom):
+        bird.dead = True
+        bird.image = 'birddead'
+
+    if not 0 < bird.y < 720:
+        bird.y = 200
+        bird.dead = False
+        bird.score = 0
+        bird.vy = 0
+        reset_pipes()
+
+
+def update():
+    update_pipes()
+    update_bird()
+
+
+def on_key_down():
+    if not bird.dead:
+        bird.vy = -FLAP_STRENGTH
+
+def button_pressed():
+    if not bird.dead:
+        bird.vy = -FLAP_STRENGTH
+###
+# HINT:
+# Add a function here which handles a button press.
+# It'll look very similar to the on_key_down() function above.
+###
+
+def draw():
+    screen.blit('background', (0, 0))
+    pipe_top.draw()
+    pipe_bottom.draw()
+    bird.draw()
+    screen.draw.text(
+        str(bird.score),
+        color='white',
+        midtop=(WIDTH // 2, 10),
+        fontsize=70,
+        shadow=(1, 1)
+    )
+
+###
+# HINT:
+# You'll need to add a handler for the button press, 
+# just like line 20 in the button.py example.
+###
+button.when_pressed = button_pressed
+
+pgzrun.go()
