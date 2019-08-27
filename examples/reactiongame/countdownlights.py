10271b11da32723fac14554efc63d1842239b31f
Joe <joeshimwell@gmail.com>
2019-08-27 15:41:52 +0100

    Changing to explanatory texts

--- examples/reactiongame/countdownlights.py
+++ examples/reactiongame/countdownlights.py
@@ -2,17 +2,23 @@ import explorerhat
 from time import sleep
 import random
 
-#turn on all the lights on the explorerHat
-explorerhat.light.yellow.on()
-explorerhat.light.blue.on()
-explorerhat.light.red.on()
+def countdown():
+    #turn on all the lights on the explorerHat
+    explorerhat.light.yellow.on()
+    explorerhat.light.blue.on()
+    explorerhat.light.red.on()
 
-#then turn them off, but with a random delay between each light for added tension / fun
-explorerhat.light.green.off()
-sleep(random.uniform(1,5))
-explorerhat.light.blue.off()
-sleep(random.uniform(1,5))
-explorerhat.light.yellow.off()
-sleep(random.uniform(1,5))
-explorerhat.light.red.off()
-explorerhat.light.green.on()
+    #then turn them off, but with a random delay between each light for added tension / fun
+    explorerhat.light.green.off()
+    sleep(random.uniform(1,5))
+
+    explorerhat.light.blue.off()
+    sleep(random.uniform(1,5))
+
+    explorerhat.light.yellow.off()
+    sleep(random.uniform(1,5))
+
+    explorerhat.light.red.off()
+    explorerhat.light.green.on()
+
+countdown()
